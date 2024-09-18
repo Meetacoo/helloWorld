d97f1f462815283613d6c05235b83744b00d33d9
jinfanghuang <2283785225@qq.com>
2024-09-18 17:29:43 +0800

    feat: print calc number

--- PrintNumber.java
+++ PrintNumber.java
@@ -3,5 +3,9 @@ public class PrintNumber {
         System.out.println(1);
         System.out.println(222);
         System.out.println(50000);
+        System.out.println(2+8);
+        System.out.println(2*8);
+        System.out.println(8 / 2);
+        System.out.println(9 % 2);
     }
 }
