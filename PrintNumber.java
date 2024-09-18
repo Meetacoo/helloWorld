501c0f747ac038154ad34377257afbd998b003a7
jinfanghuang <2283785225@qq.com>
2024-09-18 17:27:55 +0800

    feat: print number

---
+++ PrintNumber.java
@@ -0,0 +1,7 @@
+public class PrintNumber {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(222);
+        System.out.println(50000);
+    }
+}
